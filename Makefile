c4f3416f01d571e4bf719c9fc84f3f8af3db6bf4
kanokkorn <kanokorn@outlook.jp>
2022-05-04 15:13:58 +0700

    refactor Makefile, hw.h is replaceing interface

---
+++ Makefile
@@ -0,0 +1,33 @@
+TARGET_HW  = 0
+ADV_METHOD = 0
+
+CC = cc
+LD = -fuse-ld=ld
+LDLIBS = -lm
+CFLAGS = -std=c99 -Wall -Wextra -Wconversion -Wshadow\
+         -Wdouble-promotion -fno-math-errno -pedantic\
+				 -fdelete-null-pointer-checks -fno-common\
+				 -g
+
+BIN = wrbot
+SRC = main.c
+OBJ = $(SRC:.c=.o)
+
+ifeq ($(TARGET_HW), 1)
+  CFLAGS += -target aarch64 -mfpu=neon -mfloat-abi=softfp\
+						-integrated-as -DTARGET_HW
+endif
+ifeq ($(ADV_METHOD), 1)
+  CFLAGS += -DADV_METHOD
+	SRC += kalman.c
+endif
+
+all: $(OBJ) $(BIN)
+.PHONY: all clean
+
+$(OBJ): $(SRC)
+	$(CC) $(CFLAGS) $^ -c
+$(BIN) : $(OBJ)
+	$(CC) $(LD) $^ -o $@ $(LDLIBS)
+clean:
+	$(RM) *.o $(BIN)
---
+++ hw.h
@@ -0,0 +1,63 @@
+/*
+ * File: hw.h
+ * Author: kanokkorn kornkankit <kanokorn@outlook.jp>
+ */
+
+#ifdef __ARM64__
+#include <gpio.h>
+#include <gps.h>
+#include <math.h>
+#include <unistd.h>
+
+/* RASPI CHIPSET */
+#define CONSUMER "bcm"
+#define led_pin 23
+
+#define MODE_STR_NUM 4
+static char *mode_str[MODE_STR_NUM] = {
+  "n/a",
+  "None",
+  "2D",
+  "3D"
+};
+
+int gps_open(char * server, char * port, struct gps_data_t * gpsdata);
+int gps_read(struct gps_data_t * gpsdata, char * message,
+    int message_size);
+
+uint8_t check_gps_connection(void) {
+  struct gps_data_t gps_data;
+  if (0 != gps_open("localhost", "2947", &gps_data))
+    return 0;
+  else
+    return 1;
+}
+
+void raspi_setup(void){
+  struct gps_data_t gps_data;
+  const char* chipname = "gpiochip0";
+  struct gpiod_chip *chip;
+  struct gpiod_line *line;
+  chip = gpiod_chip_open_by_name(chipname);
+  (void)gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+  if (!check_gps_connection) {
+    perror("G
+  }
+  if (!chip) {
+    perror("Open chip failed\n");
+  }
+  line = gpiod_chip_get_line(chip, line_num);
+  if (!line) {
+    perror("Get line failed\n");
+  }
+  ret = gpiod_line_request_output(line, CONSUMER, 0);
+  if (ret < 0) {
+    perror("Request line as output failed\n");
+  }
+}
+void robot_signal_light(int) {
+}
+
+#endif
+
+
