7987f89a323f12988421f21daabe448d05c9e712
kanokkorn <kanokorn@outlook.jp>
2022-05-21 22:03:16 +0700

    sensible circular buffer

--- adv.h
+++ adv.h
@@ -7,33 +7,20 @@
 #include <stddef.h>
 #include <math.h>
 
-/* FIFO for avg kalman filter */
-#define Q_SIZE 10
+#define Q_ELEM 100
+#define Q_SIZE (Q_ELEM + 1)
 
-typedef struct {
-  size_t head;
-  size_t tail;
-  size_t size;
-  void** data;
-}queue_t;
+uint8_t q[Q_SIZE];
+float q_in, q_out;
 
-void* queue_read(queue_t *queue) {
-  if (queue->tail == queue->head) {
-    return NULL;
-  }
-  void* handle = queue->data[queue->tail];
-  queue->data[queue->tail] = NULL;
-  queue->tail = (queue->tail + 1) % queue->size;
-  return handle;
+void q_init (void) {
+  q_in = q_out = 0;
 }
 
-int queue_write(queue_t *queue, void* handle) {
-  if (((queue->head + 1) % queue->size) == queue->tail) {
+int q_put (float data) {
+  if (q_in == (( q_out - 1 + Q_SIZE ) % Q_SIZE))
     return -1;
-  }
-  queue->data[queue->head] = handle;
-  queue->head = (queue->head + 1) % queue->size;
-  return 0;
+  q[q_in] = data;
 }
 
 #endif /* ADV_METHOD */
