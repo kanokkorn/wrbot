cd23fdcf5740d8f08542f5884be70fc997effa01
kanokkorn <kanokorn@outlook.jp>
2022-05-24 16:32:16 +0700

    sum & avg all elements in array

--- adv.h
+++ adv.h
@@ -12,15 +12,25 @@
 
 uint8_t q[Q_SIZE];
 float q_in, q_out;
+int x, h;
+static float avg;
+static float predict;
 
 void q_init (void) {
   q_in = q_out = 0;
 }
 
 int q_put (float data) {
-  if (q_in == (( q_out - 1 + Q_SIZE ) % Q_SIZE))
-    return -1;
+  if (q_in == (( q_out - 1 + Q_SIZE ) % Q_SIZE)) {
+    q_in = 0;
+    for (x = 0; x < Q_ELEM; x++) {
+      avg += q[x];
+    }
+    avg / Q_ELEM;
+  }
   q[q_in] = data;
+  q_in = (q_in + 1) % Q_SIZE;
+  return 1;
 }
 
 #endif /* ADV_METHOD */
