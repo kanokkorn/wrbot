1388dda4b48c9cd7e9285dbd07cafdd640a94dac
kanokkorn <kanokorn@outlook.jp>
2022-05-26 00:07:40 +0700

    format with clang-format

--- adv.h
+++ adv.h
@@ -4,9 +4,9 @@
  */
 
 #ifdef ADV_METHOD
-#include <stdint.h>
-#include <stddef.h>
 #include <math.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #define Q_ELEM 100
 #define Q_SIZE (Q_ELEM + 1)
@@ -17,19 +17,17 @@ uint8_t h;
 static float avg;
 static float predict;
 
-void q_init (void) {
-  q_in = q_out = 0;
-}
+void q_init(void) { q_in = q_out = 0; }
 
-uint8_t q_put (float data) {
-  if (q_in == (( q_out - 1 + Q_SIZE ) % Q_SIZE)) {
+uint8_t q_put(float data) {
+  if (q_in == ((q_out - 1 + Q_SIZE) % Q_SIZE)) {
     q_in = 0;
     for (h = 0; h < Q_ELEM; h++) {
       avg += q[h];
     }
     avg = avg / Q_ELEM;
   }
-  q[q_in] = (uint8_t) data;
+  q[q_in] = (uint8_t)data;
   q_in = (q_in + 1) % Q_SIZE;
   return 1;
 }
